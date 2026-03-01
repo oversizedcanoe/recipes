a00ed6afcb7da3ddf5557c646ecabf8780ec030c
billy <billy.cook12345@gmail.com>
2026-03-01 14:59:47 -0500

    Fixing URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://oversizedcanoe.github.io'
+    site: 'https://oversizedcanoe.github.io',
+    base: '/recipes'
 });
 
